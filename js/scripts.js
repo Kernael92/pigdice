ea5224a1dca2d2e10aab2455869aacc6c840eb7c
Kernael92 <kernaelapuko5@gmail.com>
2018-09-28 15:15:33 +0300

    add the business logic part of the script file

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//business logic
+var Player = function(turn) {
+   this.scoreTotal = 0;
+   this.currentScore = 0;
+   this.turn = turn;
+};
+Player.prototype.takeTurn = function() {
+   if (this.turn === true) {
+      this.turn = false;
+   } else {
+      this.turn = true;
+   }
+};
+Player.prototype.roll = function() {
+   var rand = Math.floor(Math.random() * 6) + 1;
+   this.currentScore = rand;
+};
+Player.prototype.addPoints = function(rollPoints) {
+   this.scoreTotal += rollPoints;
+};
+
+var player1 = new Player(true);
+var player2 = new Player(false);
+
+var totalRoll;
